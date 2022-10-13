6566ce352b8a66f06a9411667acd63e6d1bec286
Ramon <ramonpons2011@gmail.com>
2022-10-13 12:40:43 +0200

    add auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,11 +23,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('post/all',[PostController::class,'all']); 
 Route::get('post/slug/{post:url_clean}',[PostController::class,'slug']); 
-Route::resource('post',PostController::class)->except(['create','edit']);
 
 
 Route::get('category/all',[CategoryController::class,'all']);
 Route::get('category/slug/{slug}',[CategoryController::class,'slug']); 
 Route::get('category/{id}/posts',[CategoryController::class,'posts']); 
-Route::resource('category',CategoryController::class)->except(['create','edit']); 
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::resource('post',PostController::class)->except(['create','edit']);
+    Route::resource('category',CategoryController::class)->except(['create','edit']); 
+});
+
+Route::post('user/login', [UserController::class, 'login']);
  
